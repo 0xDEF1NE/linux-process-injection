80fc040574045f9735149f9b27761257cfa9a088
DEF1NE <65466003+d3f1ne@users.noreply.github.com>
2023-05-26 05:09:25 -0300

    Update utils.c

--- ptrace/src/utils.c
+++ ptrace/src/utils.c
@@ -1,9 +1,5 @@
 #include "utils.h"
 
-/**
- * Converte um ponteiro para char em um inteiro
- * Retorna o inteiro convertido.
- */
 int _atoi(const char *s)
 {
     int n = 0, is_neg = 0;
@@ -16,8 +12,8 @@ int _atoi(const char *s)
     case '+':
         s++;
     }
-    /* Compute n as a negative number to avoid overflow on INT_MIN */
-    while (u_isdigit(*s))
+
+	while (u_isdigit(*s))
         // (52 - 48) = 4
         n = 10 * n + (*s++ - '0');
     return is_neg ? -n : n;
@@ -32,7 +28,6 @@ char *vasprintfEx(const char *fmt, va_list args)
 {
     char *p;
 
-    // Se vasprintf() falhar GARANTE que retornará NULL.
     if (vasprintf(&p, fmt, args) < 0)
         p = NULL;
 
@@ -74,26 +69,11 @@ void printShellcode(const unsigned char* shellcode, size_t size) {
     printf("\n");
 }
 
-/**
- * Verifica se o caracter é um ' ', '\t', '\n' ou '\12'
- * retorna 1 caso seja falso
- */
+
 inline int u_isspace(char c) { return (c == ' ' || c == '\t' || c == '\n' || c == '\12'); }
 
-/**
- * Verifica se o numero é maior que 0 e menor que 9
- * retorna 1 caso seja falso
- */
 inline int u_isdigit(char c) { return (c >= '0' && c <= '9'); }
 
-/**
- * Verifica se o caractere é uppercase
- * retorna 1 caso seja falso
- */
 inline int u_isupper(char c) { return (c >= 'A' && c <= 'Z'); }
 
-/**
- * Verifica se o caractere é uppercase ou normal
- * retorna 1 caso seja falso
- */
 inline int u_isalpha(char c) { return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')); }
